3c13cf9c58643f41c7dfd45dccca986b9e98c5c3
fsprogamer <fsprogamer@gmail.com>
2017-08-24 13:21:40 +0300

    урок 1

---
+++ Projects/Lesson1/WpfTestMailSender/BtnFromCode/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BtnFromCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button btn = new Button();
+            btn.FontWeight = FontWeights.Bold;
+            btn.Width = 140;
+            btn.Height = 30;
+
+            WrapPanel pnl = new WrapPanel();
+
+            TextBlock txt = new TextBlock();
+            txt.Text = "Разно";
+            txt.Foreground = Brushes.Blue;
+            pnl.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "цветная";
+            txt.Foreground = Brushes.Red;
+            pnl.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "кнопка";
+            pnl.Children.Add(txt);
+
+            btn.Content = pnl;
+            //pnlMain.Children.Add(btn);
+            mainGrid.Children.Add(btn);
+
+        }
+    }
+}
---
+++ Projects/Lesson1/WpfTestMailSender/Event/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Event
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Grid_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Координаты " + e.GetPosition(this).ToString());
+        }
+
+    }
+}
