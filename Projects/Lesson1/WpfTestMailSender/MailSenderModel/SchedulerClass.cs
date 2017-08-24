8122f4f774040716dd105e49e04b5366fcacdace
fsprogamer <fsprogamer@gmail.com>
2017-08-24 16:43:19 +0300

    model

---
+++ Projects/Lesson1/WpfTestMailSender/MailSenderModel/SchedulerClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MailSenderModel
+{
+    public class SchedulerClass
+    {
+        public void Add()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
