df9c52d623518c8ca22483efd0859f38c01114ba
fsprogamer <fsprogamer@gmail.com>
2017-09-01 13:07:07 +0300

    GoF

---
+++ Projects/GangOfFour/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    /// <summary>
+    /// Abstract Factory Design Pattern.
+    /// Интерфейс для создания групп связанных или зависящих объектов без определения конкретных классов
+    /// </summary>
+
+    internal class MainApp
+    {
+
+        public static void Main()
+        {
+            //Eurasia animal world
+            ContinentFactory eurasia = new EurasiaFactory();
+            AnimalWorld world = new AnimalWorld(eurasia);
+            world.RunFoodChain();
+            //Africa animal world
+            ContinentFactory africa = new AfricaFactory();
+            world = new AnimalWorld(africa);
+            world.RunFoodChain();
+            
+            Console.ReadKey();
+        }
+    }
+
+    internal abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+    internal class AfricaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Antelope();
+        }
+
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
+        }
+    }
+
+    internal class EurasiaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Deer();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+    }
+
+    internal abstract class Herbivore
+    {
+    }
+
+    internal abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+    #region Africa
+    internal class Antelope : Herbivore
+    {
+    }
+
+    internal class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name +
+              " ест " + h.GetType().Name);
+        }
+    }
+    #endregion
+
+    #region Eurasia
+    internal class Deer : Herbivore
+    {
+    }
+
+    internal class Wolf : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name +
+              " ест " + h.GetType().Name);
+        }
+    }
+    #endregion
+
+    internal class AnimalWorld
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
+
+        // Constructor
+        public AnimalWorld(ContinentFactory factory)
+        {
+            _carnivore = factory.CreateCarnivore();
+            _herbivore = factory.CreateHerbivore();
+        }
+
+        public void RunFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+}
---
+++ Projects/GangOfFour/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryMethod
+{
+    /// <summary>    
+    /// Factory Method Design Pattern.
+    /// Определяет интерфейс для создания объекта, позволяя дочерним классам решать какой класс создавать.
+    /// Фабричный метод позволяет перенести ответственность по конкретизации реализации в дочерние классы.
+    /// </summary>
+
+    class MainApp
+    {
+        static void Main()
+        {
+            // constructors call Factory Method
+            Car[] cars = new Car[2];
+            cars[0] = new Truck();
+            cars[1] = new Bus();
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine("\n" + car.GetType().Name + "--");
+                foreach (Part part in car.Parts)
+                {
+                    Console.WriteLine(" " + part.GetType().Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Part
+    {
+
+    }
+    class TruckTransmission : Part
+    {
+
+    }
+    class TruckEngine : Part
+    {
+
+    }
+    class TruckSuspension : Part
+    {
+
+    }
+    class BusTransmission : Part
+    {
+
+    }
+    class BusEngine : Part
+    {
+
+    }
+    class BusSuspension : Part
+    {
+
+    }
+  
+    abstract class Car
+    {
+        private List<Part> _parts = new List<Part>();
+        //call abstract Factory method
+        public Car()
+        {
+            this.CreateParts();
+        }
+        public List<Part> Parts
+        {
+            get { return _parts; }
+        }
+        // Factory Method
+        public abstract void CreateParts();
+    }
+
+    class Truck : Car
+    {
+        // Factory Method implementation
+        public override void CreateParts()
+        {
+            Parts.Add(new TruckTransmission());
+            Parts.Add(new TruckEngine());
+            Parts.Add(new TruckSuspension());
+        }
+    }
+    class Bus : Car
+    {
+        // Factory Method implementation
+        public override void CreateParts()
+        {
+            Parts.Add(new BusTransmission());
+            Parts.Add(new BusEngine());
+            Parts.Add(new BusSuspension());
+        }
+    }
+}
