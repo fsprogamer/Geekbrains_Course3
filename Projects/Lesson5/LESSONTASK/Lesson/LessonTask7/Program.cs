b095303ec3ee545da2a7bb4452d0e5637d28b4f1
fsprogamer <fsprogamer@gmail.com>
2017-09-06 16:51:26 +0300

    fix project folders

---
+++ Projects/Lesson5/LESSONTASK/Lesson/LessonTask7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    private static object lockObject = new object();
+    static void ThreadMethod(object state)
+    {
+        lock (lockObject)
+        {
+            int value = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Поток {0}: {1}", Thread.CurrentThread.Name, value);
+                value++;
+                Thread.Sleep(200);
+            }
+        }
+    }
+    static void Main()
+    {
+        ThreadPool.SetMinThreads(2, 2);
+        ThreadPool.SetMaxThreads(5, 5);
+        for (int i = 0; i < 5; i++)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadMethod));
+        }
+        Console.ReadLine();
+    }   
+}
