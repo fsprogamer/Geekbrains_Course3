c800b70d0e7667440dfa090e0265fe87150f2921
fsprogamer <fsprogamer@gmail.com>
2017-09-11 16:19:50 +0300

    addition

--- Projects/Lesson5/LESSONTASK/Lesson/LessonTask1/Program.cs
+++ Projects/Lesson5/LESSONTASK/Lesson/LessonTask1/Program.cs
@@ -3,20 +3,29 @@ using System.Threading;
 
 class Program
 {
+    //static void Main()
+    //{
+    //    Thread thread = new Thread(new ThreadStart(ThreadMethod));
+    //    thread.Name = "Вторичный поток";
+    //    //thread.Priority = ThreadPriority.Normal;
+    //    thread.Start();
+    //    Console.WriteLine("Ждём окончания работы потока.");
+    //    Console.ReadKey();
+    //}
+    //static void ThreadMethod()
+    //{
+    //    Thread.Sleep(2000);
+    //    Console.WriteLine($"{Thread.CurrentThread.Name} завершён.");
+    //}
+
     static void Main()
     {
-        Thread thread = new Thread(new ThreadStart(ThreadMethod));
+        Thread thread = new Thread(new ThreadStart(()=> { Thread.Sleep(2000); Console.WriteLine($"{Thread.CurrentThread.Name} завершён."); }));
         thread.Name = "Вторичный поток";
         //thread.Priority = ThreadPriority.Normal;
         thread.Start();
         Console.WriteLine("Ждём окончания работы потока.");
         Console.ReadKey();
     }
-    static void ThreadMethod()
-    {
-        Thread.Sleep(2000);
-        Console.WriteLine($"{Thread.CurrentThread.Name} завершён.");
-    }
-
 }
 
--- Projects/Lesson5/LESSONTASK/Lesson/LessonTask3/Program.cs
+++ Projects/Lesson5/LESSONTASK/Lesson/LessonTask3/Program.cs
@@ -6,6 +6,31 @@ public class ThreadClass
     public int SleepTime { get; set; }
     public string Message { get; set; }
 }
+//class Program
+//{
+//    static void Main()
+//    {
+//        ThreadClass threadClass = new ThreadClass
+//        {
+//            SleepTime = 2000,
+//            Message = "Поток завершен."
+//        };
+//        Thread thread = new Thread(new ParameterizedThreadStart(ThreadMethod))
+//        {
+//            Priority = ThreadPriority.Highest,
+//            Name = "Вторичный поток"
+//        };
+//        thread.Start(threadClass);
+//        Console.WriteLine("Ждем окончания работы потока.");
+//    }
+//    static void ThreadMethod(object obj)
+//    {
+//        ThreadClass threadClass = (ThreadClass)obj;
+//        Thread.Sleep(threadClass.SleepTime);
+//        Console.WriteLine(threadClass.Message);
+//    }
+//}
+
 class Program
 {
     static void Main()
@@ -15,20 +40,17 @@ class Program
             SleepTime = 2000,
             Message = "Поток завершен."
         };
-        Thread thread = new Thread(new ParameterizedThreadStart(ThreadMethod))
+        Thread thread = new Thread(new ParameterizedThreadStart((object obj)=> {
+            ThreadClass tClass = (ThreadClass)obj;
+            Thread.Sleep(tClass.SleepTime);
+            Console.WriteLine(tClass.Message);
+        }))
         {
             Priority = ThreadPriority.Highest,
             Name = "Вторичный поток"
         };
         thread.Start(threadClass);
         Console.WriteLine("Ждем окончания работы потока.");
-    }
-    static void ThreadMethod(object obj)
-    {
-        ThreadClass threadClass = (ThreadClass)obj;
-        Thread.Sleep(threadClass.SleepTime);
-        Console.WriteLine(threadClass.Message);
-    }
-
+    }  
 }
 
